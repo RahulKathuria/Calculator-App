e94392d05dd48350bbd20581aa150f176cd2cfcc
RahulKathuria <rahulkathuria52@gmail.com>
2020-09-11 03:03:59 +0530

    Added buttons

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
@@ -11,14 +9,21 @@ class MyButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(20),
-      child: Container(
-        color: buttonColor,
-        child: Center(child: Text(buttonText,style: TextStyle(
-          color:textColor
-        ),)),
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Container(
+          color: buttonColor,
+          child: Center(
+              child: Text(
+            buttonText,
+            style: TextStyle(color: textColor, fontSize: 20),
+          )),
+        ),
       ),
     );
   }
+
+  
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:calcapp/buttons.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -18,6 +19,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<String> buttons = [
+    'C',
+    'DEL',
+    '%',
+    '/',
+    '9',
+    '8',
+    '7',
+    'x',
+    '6',
+    '5',
+    '4',
+    '-',
+    '3',
+    '2',
+    '1',
+    '+',
+    '0',
+    '.',
+    'ANS',
+    '=',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,10 +51,26 @@ class _HomePageState extends State<HomePage> {
           Expanded(
               flex: 2,
               child: Container(
-                color: Colors.greenAccent,
+                child: GridView.builder(
+                    itemCount: buttons.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 4),
+                    itemBuilder: (BuildContext context, int index) {
+                      return MyButton(
+                        buttonText: buttons[index],
+                        buttonColor: isOperator(buttons[index]) ? Colors.green : Colors.greenAccent[200],
+                        textColor: isOperator(buttons[index]) ? Colors.white : Colors.green,
+                      );
+                    }),
               )),
         ],
       ),
     );
+  }
+  bool isOperator(String x) {
+    if (x == '%' || x == '/' || x == '+' || x == '-' || x == '='|| x == 'x')
+      return true;
+    else
+      return false;
   }
 }
